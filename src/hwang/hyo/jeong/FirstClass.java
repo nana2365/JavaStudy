53879a6c904c8784ee847933bd50780cc7cbca91
nana2365 <nana2365@DESKTOP-CQMR31M>
2020-10-06 20:15:18 +0900

    init

---
+++ src/hwang/hyo/jeong/FirstClass.java
@@ -0,0 +1,10 @@
+package hwang.hyo.jeong;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world"); // sysout ÀÔ·Â ÈÄ => ctrl + space
+	}
+
+}
