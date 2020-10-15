8cb86e52f55682bf4a3ac1bd8fd4d7d157324bff
user <user@DESKTOP-RPEB900>
2020-10-16 08:07:38 +0900

    commit test1

--- src/hwang/hyo/jeong/FirstClass.java
+++ src/hwang/hyo/jeong/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world"); // sysout 입력후 => ctrl + space
+		//commit test 1
 		
 		System.out.println("Good luck!!!");
 		
