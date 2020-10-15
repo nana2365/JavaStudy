f433cdee5189bba6ff9ba81bb965dfd91e41eba4
user <user@DESKTOP-RPEB900>
2020-10-16 08:04:49 +0900

    workspace home test

--- src/hwang/hyo/jeong/FirstClass.java
+++ src/hwang/hyo/jeong/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world"); // sysout �Է� �� => ctrl + space
+		System.out.println("hello world"); // sysout 입력후 => ctrl + space
 		
 		System.out.println("Good luck!!!");
 		
